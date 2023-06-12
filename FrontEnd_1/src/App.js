2cec53bc4106332c02bc10ff405d0dad80dbb2c0
lokesh matta <iamlokeshofficial@gmail.com>
2023-06-12 11:32:57 +0530

    second commit

--- FrontEnd_1/src/App.js
+++ FrontEnd_1/src/App.js
@@ -3,9 +3,9 @@ function App() {
     <div>
       <h1>Hello</h1>
       <p>Start the Server at by using Node server.js</p>
-      <p>http://localhost:8008/numbers/odd</p>
-      <p>http://localhost:8008/numbers/primes</p>
-      <p>http://localhost:8008/numbers/fibo</p>
+      <p>use this link for odd numbers from 1 - 50 : <a href="http://localhost:8008/numbers/odd"> ODD numbers</a></p>
+      <p>use this link for prime numbers from 1 - 50 :<a href="http://localhost:8008/numbers/primes"> primes numbers</a></p>
+      <p>use this link for fibonacci Series numbers from 1 - 50 : <a href="http://localhost:8008/numbers/fibo"> fibonacci Series </a></p>
     </div>
   );
 }
